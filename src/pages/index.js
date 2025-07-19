fdc8c6e6383badb3e389857ab577fe5e0a852bad
Souradeep Singha <84996170+souradeepsingha@users.noreply.github.com>
2025-07-19 19:26:07 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,21 +10,25 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Curious to here more
-          </Link>
-        </div>
-      </div>
-    </header>
+  <header
+  className={clsx('hero', styles.heroBanner)}
+  style={{ backgroundColor: '#4B45A1' }}
+>
+  <div className="container">
+    <Heading as="h1" className="hero__title">
+      {siteConfig.title}
+    </Heading>
+    <p className="hero__subtitle">{siteConfig.tagline}</p>
+    <div className={styles.buttons}>
+      <Link
+        className="button button--secondary button--lg"
+        to="/docs/intro">
+        Curious to here more
+      </Link>
+    </div>
+  </div>
+</header>
+
   );
 }
 
